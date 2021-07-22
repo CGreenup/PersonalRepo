20c3fa846fdf3b1d3e6ddb70e0ac2823ce6518b5
June Greenup <cgreenup2000@gmail.com>
2021-07-22 14:44:52 -0500

    Another change!

--- src/main/java/com/revature/servlets/HelloServlet.java
+++ src/main/java/com/revature/servlets/HelloServlet.java
@@ -18,7 +18,7 @@ public class HelloServlet extends HttpServlet {
 		
 		//Responses come with a method to create a Print Writer to write the body of the response
 		PrintWriter pw = response.getWriter();
-		pw.print("<h1>Hello from that old tested doGet method!</h1>");
+		pw.print("<h1>Hello from that Jenkin's tested doGet method!</h1>");
 		
 		return;
 	}
